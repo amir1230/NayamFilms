601e0e08fe746146cfff2dbefa0452c4d7f6e40c
AbdulWahabRaza123 <abdulwahabraza123@gmail.com>
2024-05-23 18:30:05 +0500

    portfolio has been added

--- src/components/home/Portfolio.jsx
+++ src/components/home/Portfolio.jsx
@@ -1,5 +1,35 @@
 import Image from "next/image";
 import React from "react";
+const col1Portfolio = [
+  {
+    src: "/assets/portfolio/col-1-1.svg",
+  },
+  {
+    src: "/assets/portfolio/col-1-2.svg",
+  },
+  {
+    src: "/assets/portfolio/col-1-3.svg",
+  },
+];
+const col2Portfolio = [
+  {
+    src: "/assets/portfolio/col-2-1.svg",
+  },
+  {
+    src: "/assets/portfolio/col-2-2.svg",
+  },
+];
+const col3Portfolio = [
+  {
+    src: "/assets/portfolio/col-3-1.svg",
+  },
+  {
+    src: "/assets/portfolio/col-3-2.svg",
+  },
+  {
+    src: "/assets/portfolio/col-3-3.svg",
+  },
+];
 
 const Portfolio = () => {
   return (
@@ -16,6 +46,51 @@ const Portfolio = () => {
         malesuadlorem odio enim. Imperdiet mauris iaculis velit mauris.
         Consequat quam neque maecenas nisi odio sed auctor etiam morbi.{" "}
       </p>
+
+      <div className="flex flex-row items-center justify-center gap-3 max-md:flex-wrap mt-5">
+        <div className="flex flex-col items-center gap-3 max-md:w-full">
+          {col1Portfolio.map((item) => {
+            return (
+              <Image
+                key={item.src}
+                src={item.src}
+                alt="portfolio"
+                width={324}
+                height={100}
+                className="object-cover max-md:w-full"
+              />
+            );
+          })}
+        </div>
+        <div className="flex flex-col items-center gap-3 max-md:w-full">
+          {col2Portfolio.map((item) => {
+            return (
+              <Image
+                key={item.src}
+                src={item.src}
+                alt="portfolio"
+                width={350}
+                height={200}
+                className="object-cover max-md:w-full"
+              />
+            );
+          })}
+        </div>
+        <div className="flex flex-col items-center gap-3 max-md:w-full">
+          {col3Portfolio.map((item) => {
+            return (
+              <Image
+                key={item.src}
+                src={item.src}
+                alt="portfolio"
+                width={260}
+                height={100}
+                className="object-cover max-md:w-full"
+              />
+            );
+          })}
+        </div>
+      </div>
     </div>
   );
 };
